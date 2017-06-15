f3482cc772b3f2faafd6ed4991c0357fd6df84dd
Garrett Haas <garrett.l.haas@gmail.com>
2017-06-14 22:04:16 -0500

    building in validation

--- main.py
+++ main.py
@@ -20,29 +20,29 @@ form = """<!DOCTYPE html>
                 <tr>
                     <td><label for="username">Username</label></td>
                     <td>
-                        <input name="username" type="text" value="">
-                        <span class="error"></span>
+                        <input name="username" type="text" value='{username}'>
+                        <span class="error">{username_error}</span>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="password">Password</label></td>
                     <td>
-                        <input name="password" type="password">
-                        <span class="error"></span>
+                        <input name="password" type="password" value='{password}'>
+                        <span class="error">{password_error}</span>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="verify">Verify Password</label></td>
                     <td>
-                        <input name="verify" type="password">
-                        <span class="error"></span>
+                        <input name="verify" type="password" value='{verify}'>
+                        <span class="error">{verify_error}</span>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="email">Email (optional)</label></td>
                     <td>
-                        <input name="email" type="email" value="">
-                        <span class="error"></span>
+                        <input name="email" type="email" value='{email}'>
+                        <span class="error">{email_error}</span>
                     </td>
                 </tr>
             </table>
@@ -54,5 +54,38 @@ form = """<!DOCTYPE html>
 @app.route("/")
 def index():
     return form.format("")
+
+@app.route("/validate-form", methods = ['POST'])
+def validate-form():
+    return validate-form.format(username ='',username_error='',password='',password_error='',verify='',verify_error='',emai='', email_error='')
+
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
+
+    
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+#If the user's form submission is not valid, you should reject it and re-render the form with some feedback to inform the user of what they did wrong. The following things should trigger an error:
+
+        #The user leaves any of the following fields empty: username, password, verify password.
+
+        #The user's username or password is not valid -- for example, it contains a space character or it consists of less than 3 characters or more than 20 characters (e.g., a username or password of "me" would be invalid).
+
+        #The user's password and password-confirmation do not match.
+
+        #The user provides an email, but it's not a valid email. Note: the email field may be left empty, but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
+    
+#Each feedback message should be next to the field that it refers to.
+
+#For the username and email fields, you should preserve what the user typed, so they don't have to retype it. With the password fields, you should clear them, for security reasons.
+
+#If all the input is valid, then you should redirect the user to a welcome page that uses the username input to display a welcome message of: "Welcome, [username]!"
+
+#Use templates (one for the index/home page and one for the welcome page) to render the HTML for your web app.
 
 app.run()
